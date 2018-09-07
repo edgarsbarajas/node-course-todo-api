0efa7f06e3fe8822f16e53f2e41f93c1dc4b8f67
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-09-06 21:01:50 -0700

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/ToDoApp", { useNewUrlParser: true }, (error, client) => {
+  if(error){
+    return onsole.log('error!*****:\n', error);
+  }
+  console.log('Connected to MongoDB server.');
+
+  const db = client.db('TodoApp');
+
+  // deleteMany - deletes all that fits argument
+  // db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // deleteOne - finds the first instance, deletes it
+  // db.collection('Todos').deleteOne({ text: 'eat lunch'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // findOneAndDelete - finds one, deletes it, returns the document (cool)
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+  //   console.log(result);
+  // })
+
+  // db.collection('Users').deleteMany({name: 'Edgar Barajas'}).then((result) => {
+  //   console.log(result);
+  // })
+
+  db.collection('Users').findOneAndDelete({_id: new ObjectID('5b8f4573501498063b897c5c')});
+  // client.close();
+})
