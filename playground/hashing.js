cf77404a90da3ab93d1a04480a990df3113643aa
Edgar Barajas <edgarsbarajas@yahoo.com>
2018-10-03 13:04:25 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log('token: ', token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded);
+var hashedPassword = '$2a$10$1wDDl6uoRob0DCrLIyUzDOfhJjOQgNPnmcyvUdZV/fogvxC7TpkWO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log('token: ', token);
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded);
 
 // jwt.verify();
 
--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,14 +1,11 @@
 var {User} = require('../models/user');
 
 var authenticate = (req, res, next) => {
-  console.log("AUTH!!!!!!!!!!!!!!!")
   var token = req.header('x-auth');
   console.log(token);
 
   User.findByToken(token).then((user) => {
-    console.log("found token!")
     if(!user){
-      console.log("NO USER!");
       return new Promise.reject();
     }
 
